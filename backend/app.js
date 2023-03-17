f0c2c6d00c1d38c7b22d3954c4449a7d3e66191b
Jean Atsu OSSEYI <jean.osseyi@laposte.net>
2023-03-17 10:51:51 +0100

    Projet 6 - Middleware

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,6 @@ const userRoutes = require("./routes/user");
 
 const app = express();   
 
-//database
-mongoose.connect('mongodb+srv://JeanAtsu:Oss117_01234@jeanatsu.ccc0yk8.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !')); 
-
-
 //CORS (Cross origin)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,6 +13,13 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+  //database
+mongoose.connect('mongodb+srv://JeanAtsu:Oss117_01234@jeanatsu.ccc0yk8.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !')); 
 
 app.use(express.json()); //Pour exposer le body des requêtes
 app.use('/api/sauce', sauceRoutes);
